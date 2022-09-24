ac7016efc03507dd016f7e595d90c176fc7b31ad
Tiago Krebs <krebstiago@gmail.com>
2022-09-24 10:16:43 -0300

    spacemacs test

--- .config/gnu.emacs/emacs_install.sh
+++ .config/gnu.emacs/emacs_install.sh
@@ -1,16 +1,18 @@
 #/bin/bash
 
-VERSION='28.1'
+VERSION='28.2'
+export CC="gcc-10"
 
 # apt
-# sudo apt-get install libxaw7-dev libjpeg-dev libgif-dev libtiff-dev gnutls-bin libgnutls28-dev
+sudo apt-get install gcc-10 libgccjit0 libgccjit-10-dev libxft-dev libcairo2-dev mailutils libxaw7-dev libjpeg-dev libgif-dev libtiff-dev gnutls-bin libgnutls28-dev
 
 wget https://ftp.gnu.org/gnu/emacs/emacs-$VERSION.tar.xz
 tar -xf emacs-$VERSION.tar.xz
 cd emacs-$VERSION
-./configure
+./configure --with-json --with-native-compilation --with-cairo
 make
 sudo make install
+cd ..
 rm -rf emacs-$VERSION emacs-$VERSION.tar.xz
 
 which emacs
